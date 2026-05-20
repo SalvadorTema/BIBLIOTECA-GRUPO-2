aeb6c89330cf5a73edea9cf31de8ab8efc266c18
Salvador <stemas@miumg.edu.gt>
2026-05-19 22:01:35 -0700

    Book.java code

--- src/com/library/model/Book.java
+++ src/com/library/model/Book.java
@@ -1,11 +1,23 @@
 package com.library.model;
 
-public class Book {
+public class Book extends AbstractMaterial {
+    private String author;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public Book(String id, String title, String author) {
+        super(id, title);
+        this.author = author;
+    }
 
-	}
+    public Book(String id, String title) {
+        super(id, title);
+        this.author = "Unknown Author";
+    }
 
+    @Override
+    public int getMaxLoanDays() {
+        return 15;
+    }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 }
-//
