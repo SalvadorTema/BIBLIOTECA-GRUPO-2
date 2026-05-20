52ccbe1aa58f77218568ed6221a3d3fc25596eaf
Salvador <stemas@miumg.edu.gt>
2026-05-19 21:05:28 -0700

    AbstractMaterial.java

--- src/com/library/model/AbstractMaterial.java
+++ src/com/library/model/AbstractMaterial.java
@@ -1,10 +1,25 @@
 package com.library.model;
 
-public class AbstractMaterial {
+public abstract class AbstractMaterial {
+    private String id;
+    private String title;
+    private boolean isAvailable;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public AbstractMaterial(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isAvailable = true;
+    }
 
-	}
+    public abstract int getMaxLoanDays();
 
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public boolean isAvailable() { return isAvailable; }
+    public void setAvailable(boolean available) { this.isAvailable = available; }
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + title + " (Available: " + isAvailable + ")";
+    }
 }
